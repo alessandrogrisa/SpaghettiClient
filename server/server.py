e17890bf460340d2cc03d55640848b6333a3bd56
Alessandro G <47177441+alessandrogrisa@users.noreply.github.com>
2020-01-31 10:41:03 +0100

    History, autocomplete added

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import BaseHTTPServer
+import readline
 
 HOST = '10.200.32.131'
 PORT = 80
@@ -8,8 +9,9 @@ PORT = 80
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_GET(s):
+
         cmd = raw_input('{}{}\\>{} '.format(bcolors.OKGREEN, s.headers['Current-Location'], bcolors.ENDC))
-        if cmd == 'terminate':
+        if cmd == 'exit':
             print('{}[!] Connection closed. Press ^C to shutdown the server.'.format(bcolors.WARNING))
         s.send_response(200)
         s.send_header('Content-type', 'text/html')
@@ -31,6 +33,24 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
     def log_message(self, format, *args):
         return
 
+def autocomplete(text,state):
+    dictionary = ['download', 'exit', 'help', 'powershell', 'session', 'weapon']
+    w_dictionary = ['BasicEnum', 'foo1', 'foo2', 'foo3', 'foo4', 'foo5', 'foo6', 'foo7', 'foo8', 'foo9', 'foo10', 'foo11', 'foo12', 'foo13', 'foo14', 'foo15', 'foo16', 'foo17', 'foo18']
+
+    if text.startswith('weapon '):
+        text = text[7:]
+        results = [x for x in w_dictionary if x.startswith(text)] + [None]
+        results[state] = 'weapon ' + results[state]
+    else:
+        results = [x for x in dictionary if x.startswith(text)] + [None]
+    return results[state]
+
+
+def setup_Readline():
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer_delims('')
+    readline.set_completer(autocomplete)
+
 # Colors
 class bcolors:
     OKGREEN = '\033[92m'
@@ -42,6 +62,8 @@ class bcolors:
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST, PORT), Handler)
+
+    setup_Readline()
 
     try:
         httpd.serve_forever()
